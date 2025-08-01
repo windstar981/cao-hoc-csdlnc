8a354b401e4041c8fec5adfb23d7ab20820e83e3
nmhieu <hackviet98@gmail.com>
2025-08-01 21:53:11 +0700

    up lai code nop bai

---
+++ README.md
@@ -0,0 +1,7 @@
+## Cách chạy
+```
+git clone https://github.com/windstar981/cao-hoc-csdlnc.git
+cd project
+npm install
+npm run dev
+```
---
+++ server/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const chalk = require('chalk');
+const cors = require('cors');
+const helmet = require('helmet');
+const multer = require('multer');
+const path = require('path');
+const keys = require('./config/keys');
+const routes = require('./routes');
+const socket = require('./socket');
+const setupDB = require('./utils/db');
+
+const { port } = keys;
+const app = express();
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    frameguard: true
+  })
+);
+app.use(cors());
+
+setupDB();
+require('./config/passport')(app);
+app.use(routes);
+
+const server = app.listen(port, () => {
+  console.log(
+    `${chalk.green('✓')} ${chalk.blue(
+      `Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`
+    )}`
+  );
+});
+
+socket(server);
